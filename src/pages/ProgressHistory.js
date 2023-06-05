08dc94dadde85347f31f91443f1508c8ea07b7d8
Bassil <116284488+Bassil88@users.noreply.github.com>
2023-06-05 10:25:52 +0200

    Update ProgressHistory.js
    final commit

--- src/pages/ProgressHistory.js
+++ src/pages/ProgressHistory.js
@@ -66,12 +66,16 @@ const ProgressHistory = () => {
 
   return (
     <Box className="bg-transparent p-4 flex flex-col items-center justify-center ">
+
       <Box className="flex justify-between mb-4 bg-gradient-to-tl from-sky-900 via-gray-900 bg-opacity-80 rounded-xl">
+
         <Box className="flex-1 m-4">
+        
           <Typography variant="h5" style={{ color: 'deepPink', fontWeight:'bolder' }} >
             <FaDumbbell className=" m-4 "/>
             Total Exercises per Category
           </Typography>
+
           <LineChart width={600} height={600} data={exerciseChartData}>
             <XAxis dataKey="name" stroke="yellow" />
             <YAxis stroke="yellow" />
@@ -81,6 +85,7 @@ const ProgressHistory = () => {
             <Line type="monotone" dataKey="exercises" stroke="skyblue" fill="white" />
           </LineChart>
         </Box>
+
         <Box className="flex-1 mr-6 ">
           <Typography variant="h5" style={{ color: 'deepPink', marginBottom: '1rem' , fontWeight:'bolder'}}>
             <GiFire className="m-4 "/>
@@ -96,12 +101,14 @@ const ProgressHistory = () => {
           </BarChart>
         </Box>
       </Box>
+
       <Box>
         <Typography variant="h4" className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500 ">
           <GiWeightLiftingUp className="mr-2 text-4xl text-white" />
           Total Exercises: {totalExercises}
         </Typography>
       </Box>
+
     </Box>
   );
 };
